59313c4cd9141a2e726a8da08a95c8a7f271f2a1
pelumi-guy <polalekan@gmail.com>
2024-04-12 19:17:12 +0100

    added padding for better responsiveness

--- src/components/landing/ThirdSection.tsx
+++ src/components/landing/ThirdSection.tsx
@@ -6,7 +6,7 @@ import styles from "../../app/page.module.css";
 const ThirdSection = () => {
   return (
     <div className={`${styles.thirdSection} container-fluid`}>
-        <div className="row mb-md-4">
+        <div className="row mb-md-5 py-md-3">
             <div className="col-12 col-md-5">
                 <Image src={images.SnoopDance} alt="snoop dancing gif" className={`${styles.snoopDanceGif} img-fluid`}/>
             </div>
@@ -19,7 +19,7 @@ const ThirdSection = () => {
             </div>
         </div>
 
-        <div className="row mt-md-4">
+        <div className="row mt-md-5 py-md-3">
             <div className={`${styles.gradientBorder2} col-12 col-md-7`}>
                 <div className={`${styles.gradientBorderFill}`}>
                     <Image src={images.AIUtilities} alt="utilities" />
